c308f7307d09e9517f5bbc5abc801771af67ac48
ingyinnhtike98@gmail.com <ingyinnhtike98@gmail.com>
2021-10-28 17:30:52 +0630

    adding Admin LTE

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('client', ClientController::class);
+
+Auth::routes();
